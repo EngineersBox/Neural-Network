109e36bec665e04f850eec5407313334a273f061
EngineersBox <toastcraft.info@gmail.com>
2019-11-08 14:01:05 +1100

    Moved constant definition to __name__ section and added write up about neural networks (perceptron)

--- perceptron.py
+++ perceptron.py
@@ -1,13 +1,14 @@
 import numpy as np
 
-TRAINING_ITERATIONS = 100000
 SEED = 1
-np.set_printoptions(formatter={'all':lambda x: str(x)})
+
+np.set_printoptions(formatter={'all': lambda x: str(x)})
 
 class NN:
 
     def __init__(self, seed=SEED):
         np.random.seed(seed)
+        # Give the synapses random weightings initially
         self.synaptic_weights = 2 * np.random.random((3, 1)) - 1
 
     def __sigmoid(self, x):
@@ -16,17 +17,22 @@ class NN:
     def __sigmoid_derivative(self, x):
         return self. __sigmoid(x) * (1 - self. __sigmoid(x))
 
-    def train(self, t_inputs, t_outputs, iters=TRAINING_ITERATIONS):
+    def train(self, t_inputs, t_outputs, iters):
         for iterations in range(iters):
+            # Calculate what the output could be given current understanding (weightings)
             output = self.think(t_inputs)
+            # See how much the guess differs from the actual answer
             error = t_outputs - output
+            # Improve understanding of the data by changing the weighting values of synapses
             self.synaptic_weights += np.dot(t_inputs.T, error * self.__sigmoid_derivative(output))
 
     def think(self, inputs):
+        # Calculate a guessed output as a product of the inputs and weights
         return self.__sigmoid(np.dot(inputs, self.synaptic_weights))
 
-
 if __name__ == "__main__":
+
+    TRAINING_ITERATIONS = 100000
 
     neural_network = NN()
     print("Random starting weights:\n", neural_network.synaptic_weights)
